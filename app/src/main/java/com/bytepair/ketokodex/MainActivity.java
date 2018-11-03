7b30c2343228837b403e1a8511a7a444eea833ab
Byte Pair <bytepair@gmail.com>
2018-11-03 19:08:30 -0400

    Add icon and style

--- app/src/main/java/com/bytepair/ketokodex/MainActivity.java
+++ app/src/main/java/com/bytepair/ketokodex/MainActivity.java
@@ -45,9 +45,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
